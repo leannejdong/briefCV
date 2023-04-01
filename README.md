ce3d4b139061e59dad61c87f5e73140dff455732
Leanne J Dong <jdleanne@gmail.com>
2023-04-01 18:40:32 +1000

    simply made in rust

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
-# Leanne Dong
+# Dr Leanne Dong
 
-Email: jdleanne@levynoise.com
+Email: jdleanne@gmail.com
 
-Education: [
-    "PhD in Mathematics, Sydney University",
-    "Bachelor of Science in Mathematics and Statistic (Hon 1, Medal), UTS",
-    "Master of Science in Finance, UNSW",
-    "Bachelor of Art and Business, Macquarie University ",
-]
+**Education:**
 
-Work: [
-    "Independent Software Engineer at SpaceTimeNoise, 2022-Current",
-    "C++ Linux developer at tonicbits, 2022",
-    "Postdoctoral researcher in Computer Science and Engineering at UTS and Concordia Uni, 2019-2022",
-    "Casual Lecturer and Tutor (UTS, USYD, ACU), 2010-2019",
-]
+PhD in Mathematics, Sydney University (2018)
 
-Skills:
+Bachelor of Science in Mathematics and Statistic (Hon 1, Medal), UTS
+
+Master of Science in Finance, UNSW
+
+Bachelor of Art and Business, Macquarie University
+
+
+**Work:**
+
+Independent Software Engineer at SpaceTimeNoise, 2022-Current
+
+C++ Linux developer at tonicbits, 2022
+
+Postdoctoral researcher in Computer Science and Engineering at UTS and Concordia Uni, 2019-2022
+
+Casual Lecturer and Tutor (UTS, USYD, ACU), 2010-2019
+
+
+**Skills:**
 
 - C++
+- JUCE
+- Boost
+- Qt
+- C
 - Rust
 - Python
 - R
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
+
+
 use std::fs::OpenOptions;
 use std::io::Write;
 
 fn main() {
     // Set up my data for the CV
-    let name = "Leanne Dong";
-    let email = "jdleanne@levynoise.com";
-    let education = format!(
-        "{:#?}",
-    vec!["PhD in Mathematics, Sydney University",
-    "Bachelor of Science in Mathematics and Statistic (Hon 1, Medal), UTS",
-    "Master of Science in Finance, UNSW",
-    "Bachelor of Art and Business, Macquarie University "]);
+    let name = "Dr Leanne Dong";
+    let email = "jdleanne@gmail.com";
+    let education = vec![
+        "PhD in Mathematics, Sydney University (2018)\n",
+        "Bachelor of Science in Mathematics and Statistic (Hon 1, Medal), UTS\n",
+        "Master of Science in Finance, UNSW\n",
+        "Bachelor of Art and Business, Macquarie University\n",
+    ]
+    .join("\n");
 
-    let work= format!(
-        "{:#?}",
-        vec!["Independent Software Engineer at SpaceTimeNoise, 2022-Current",
-    "C++ Linux developer at tonicbits, 2022",
-    "Postdoctoral researcher in Computer Science and Engineering at UTS and Concordia Uni, 2019-2022",
-    "Casual Lecturer and Tutor (UTS, USYD, ACU), 2010-2019"
-    ]);
+    let work = vec![
+        "Independent Software Engineer at SpaceTimeNoise, 2022-Current\n",
+        "C++ Linux developer at tonicbits, 2022\n",
+        "Postdoctoral researcher in Computer Science and Engineering at UTS and Concordia Uni, 2019-2022\n",
+        "Casual Lecturer and Tutor (UTS, USYD, ACU), 2010-2019\n",
+    ]
+    .join("\n");
+
     let skills = vec![
         "C++",
+        "JUCE",
+        "Boost",
+        "Qt",
         "C",
         "Rust",
         "Python",
@@ -36,31 +43,26 @@ fn main() {
         "Mathematics",
         "Statistics",
         "Machine Learning",
-        "Data Analytics"
+        "Data Analytics",
     ];
+
+    let skills_str = skills
+        .iter()
+        .map(|s| format!("- {}\n", s))
+        .collect::<String>();
 
     // Format the data as a Markdown string
     let markdown = format!(
-        "# {}\n\nEmail: {}\n\nEducation: {}\n\nWork: {}\n\nSkills:\n\n{}",
-        name,
-        email,
-        education,
-        work,
-        skills
-            .iter()
-            .map(|s| format!("- {}\n", s))
-            .collect::<String>()
+        "# {}\n\nEmail: {}\n\n**Education:**\n\n{}\n\n**Work:**\n\n{}\n\n**Skills:**\n\n{}",
+        name, email, education, work, skills_str
     );
 
     // Write the Markdown string to a file
-    // let mut file = File::create("../README.md").unwrap();
-    // file.write_all(markdown.as_bytes()).unwrap();
-
     let mut file = OpenOptions::new()
-    .write(true)
-    .truncate(true)
-    .create(true)
-    .open("../README.md")
-    .unwrap();
-file.write_all(markdown.as_bytes()).unwrap();
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("../README.md")
+        .unwrap();
+    file.write_all(markdown.as_bytes()).unwrap();
 }
